7ea8a47cf309087d0cf62f43159e3ba115c8add4
Thaliaio <contatothalitanunes@gmail.com>
2022-10-10 16:39:23 -0300

    :recycle: refactor: organizing folders

--- src/components/Search.test.js
+++ src/components/Search.test.js
@@ -1,6 +1,8 @@
 import { render, screen } from "@testing-library/react";
 import Search from "./Search";
 
+ReactModal.setAppElement(document.createElement("div"));
+
 describe("Search Component", () => {
   test("Deve iniciar o input com o valor vazio", () => {
     render(<Search />);
@@ -9,3 +11,7 @@ describe("Search Component", () => {
     expect(searchInput).toBeInTheDocument();
   });
 });
+
+// se o imput existe
+// disparar evento no input
+// testar se ele busca algo não localizado
